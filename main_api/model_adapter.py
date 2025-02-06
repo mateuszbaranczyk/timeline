3c578c9acac54e5a9232f1820789f31bc2b40027
kundziorro <grzybek2413@gmail.com>
2025-02-06 18:34:56 +0100

    handle model exeptions

--- main_api/model_adapter.py
+++ main_api/model_adapter.py
@@ -3,12 +3,14 @@ import os
 
 
 class ModelAdapter:
-    domain = os.getenv("MODEL_DOMAIN")
+    domain = os.getenv("MODEL_DOMAIN", "http://localhost:3000")
 
     def summarize(self, text: str) -> str:
         endpoint = "/summarize"
         url = self.domain + endpoint
-        response = requests.get(url, json={"text": text})
+        if not text:
+            return ""
+        response = requests.post(url, json={"text": text})
         if response.status_code != 200:
-            raise Exception(f"Failed to summarize text: {response.text}")
+            return ""
         return response.json()["summary"]
