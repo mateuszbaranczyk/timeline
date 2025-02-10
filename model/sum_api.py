c8236b36e5e73c8a1facbd9e25e1bca2bc8882cb
kundziorro <grzybek2413@gmail.com>
2025-02-10 23:36:06 +0100

    add CORS middleware and error logging for text summarization in sum_api

--- model/sum_api.py
+++ model/sum_api.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import pipeline
+from fastapi.middleware.cors import CORSMiddleware
+from logging import getLogger
 
 app = FastAPI()
 summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
+log = getLogger(__name__)
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class TextRequest(BaseModel):
@@ -12,5 +23,12 @@ class TextRequest(BaseModel):
 
 @app.post("/summarize/")
 async def summarize_text(request: TextRequest):
-    summary = summarizer(request.text, max_length=256, min_length=50, do_sample=False)
+    try:
+        summary = summarizer(
+            request.text, max_length=256, min_length=50, do_sample=False
+        )
+    except Exception:
+        log.error(f"Summarizing text of length {len(request.text)}")
+        log.error("Error summarizing text")
+        return {"summary": "text too long"}
     return {"summary": summary[0]["summary_text"]}
